f36fe1aa3d846eba338d362586bbe3265a1e5f7d
sleo <leo.sautron@epitech.eu>
2023-02-03 16:09:31 +0400

    first and last

---
+++ Minecraft/src/main/java/com/github/ElPepitaux/Minecraft/PluginNamePlugin.java
@@ -0,0 +1,14 @@
+package com.github.ElPepitaux.Minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginNamePlugin extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
